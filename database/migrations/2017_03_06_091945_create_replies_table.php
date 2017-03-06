82f93355d94260b33428a66a754faae9511804a4
student <student@Win64-57.ue.loc>
2017-03-06 10:39:47 +0100

    06032017 - commit [tables migration added]

--- database/migrations/2017_03_06_091945_create_replies_table.php
+++ database/migrations/2017_03_06_091945_create_replies_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateRepliesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,12 +12,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('replies', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('comment_id')->unsigned();
+            $table->foreign('comment_id')->references('id')->on('comments');
+            $table->text('content');
             $table->timestamps();
         });
     }
@@ -29,6 +28,9 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        if(Schema::hasTable('replies')){
+            
+            Schema::drop('replies');
+        }
     }
 }
