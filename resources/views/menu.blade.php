afcbc15da183a9958054e75ba68568aba2d98990
student <student@Win64-42.ue.loc>
2017-04-03 09:37:57 +0200

    03042017 commit [responsiveness]

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,6 +1,7 @@
-<nav class='main-menu'>
-    <ul>
+<nav class="col-sm-8 main-menu">
+          <ul>
         <a href="{{route('category.get')}}"><li class='left'> Categories </li> </a><br>
         <!--  <a href="{{route('article.get')}}"> Articles </a> !-->
     </ul>
+    
 </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,21 +2,27 @@
 <html>
     
     <head>
-        <meta charset="UTF-8"> </head>
+        <meta charset="UTF-8"> 
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
     <link rel="stylesheet" href="css/bootstrap.min.css" > 
-    <body>
+    </head>
+    
+    <body class="container-fluid">
+        <div class="row">
+            <div class="col-sm-2"></div>
+            
         @include('menu')
-        <section class='main-section'>
-            <header> </header>
-            <article> 
-
-               @yield('content')
-
-
-
-
-            </article>
+        
+        <div class="row"></div>
+        </div>
+         
+        <section class="row">
+          
+           <div class="col-sm-2"></div> 
+<div class="col-sm-8">
+            <article class="main-section">@yield('content')</article>
+</div>
+            <div class="col-sm-2"></div> 
 
         </section>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
