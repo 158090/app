f5fc45f0137d84008eeaed8e6c9859c75a555159
student <student@Win64-42.ue.loc>
2017-04-03 08:58:05 +0200

    03042017 commit [bootstrap library added]

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,10 @@
 <!Doctype html>
 <html>
+    
     <head>
         <meta charset="UTF-8"> </head>
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
+    <link rel="stylesheet" href="css/bootstrap.min.css" > 
     <body>
         @include('menu')
         <section class='main-section'>
@@ -17,5 +19,7 @@
             </article>
 
         </section>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
